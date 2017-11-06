357a606c1e4c720e8ba9d922ad12d310caecf4f4
Hzj_jie <hzj_jie@hotmail.com>
2017-11-05 21:16:23 -0800

    Use shared.TelephonyState

--- src/main/java/org/gemini/mobile_network_mode/MobileNetworkActivity.java
+++ src/main/java/org/gemini/mobile_network_mode/MobileNetworkActivity.java
@@ -4,165 +4,78 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Process;
 import android.net.Uri;
-import android.telephony.TelephonyManager;
 import android.util.Log;
-import java.lang.reflect.Method;
+import java.util.Arrays;
+import org.gemini.shared.TelephonyState;
 
-public class MobileNetworkActivity extends Activity
-{
-    private static final String TAG = "Gemini-MobileNetwork";
+public class MobileNetworkActivity extends Activity {
+  private static final String TAG = "Gemini.MobileNetwork";
+  private TelephonyState state;
 
-    @Override
-    protected final void onCreate(Bundle bundle)
-    {
-        super.onCreate(bundle);
-        execute();
-        finish();
+  @Override
+  protected final void onCreate(Bundle bundle) {
+    super.onCreate(bundle);
+    execute();
+    finish();
+  }
+
+  @Override
+  protected final void onDestroy() {
+    super.onDestroy();
+    Process.killProcess(Process.myPid());
+    System.exit(0);
+  }
+
+  // Uri format = #1.
+  protected int networkType() {
+    if (getIntent() == null) {
+      Log.e(TAG, "No intent attached.");
+      return -1;
     }
 
-    @Override
-    protected final void onDestroy()
-    {
-        super.onDestroy();
-        Process.killProcess(Process.myPid());
-        System.exit(0);
+    Uri uri = getIntent().getData();
+    if (uri == null) {
+      Log.e(TAG, "No uri attached.");
+      return -1;
     }
 
-    // Uri format = #1.
-    protected int networkType()
-    {
-        if (getIntent() == null)
-        {
-            Log.e(TAG, "No intent attached.");
-            return -1;
-        }
+    try {
+      return Integer.parseInt(uri.getFragment());
+    }
+    catch (Exception ex) {
+      return -1;
+    }
+  }
 
-        Uri uri = getIntent().getData();
-        if (uri == null)
-        {
-            Log.e(TAG, "No uri attached.");
-            return -1;
-        }
+  protected int[] networkTypes() {
+    return new int[] { networkType() };
+  }
 
-        try
-        {
-            return Integer.parseInt(uri.getFragment());
-        }
-        catch (Exception ex)
-        {
-            return -1;
-        }
+  private void execute() {
+    state = new TelephonyState(this);
+    int[] types = networkTypes();
+    if (types == null || types.length == 0) {
+      Log.e(TAG, "No valid network type received.");
+      return;
     }
 
-    protected int[] networkTypes()
-    {
-        return new int[] { networkType() };
+    if (execute(types)) {
+      Log.w(TAG,
+          "Succeeded setting mobile network type to " + Arrays.toString(types));
+    } else {
+      Log.e(TAG,
+          "Failed to set mobile network type to " + Arrays.toString(types));
     }
+  }
 
-    private void execute()
-    {
-        int[] types = networkTypes();
-        if (types == null || types.length == 0)
-        {
-            Log.e(TAG, "No valid network type received.");
-            return;
-        }
-
-        boolean r = execute(0, types) ||
-                    execute(1, types);
-        if (r)
-        {
-            Log.w(TAG, "Done.");
-        }
-        else
-        {
-            Log.e(TAG, "Failed: " +
-                "None of the modes can be applied to the system.");
-        }
+  private boolean execute(int[] types)
+  {
+    Log.w(TAG, "Current type: " + state.preferredNetworkType());
+    for (int type : types) {
+      if (state.setPreferredNetworkType(type)) {
+        return true;
+      }
     }
-
-    private boolean execute(int method, int[] types)
-    {
-        try
-        {
-            Log.w(TAG, "Current type: " +
-                (Integer)(method == 0 ? report1() : report2()));
-        }
-        catch (Exception ex)
-        {
-            Log.e(TAG, "Failed to retrieve current type: " + ex.toString());
-        }
-
-        for (final int type : types)
-        {
-            try
-            {
-                boolean result =
-                    (Boolean)(method == 0 ? execute1(type) : execute2(type));
-                Log.w(TAG, "Result of applying " + type + ": " + result);
-                if (result)
-                {
-                    return true;
-                }
-            }
-            catch (Exception ex)
-            {
-                Log.e(TAG, "Failed to execute command: " + ex.toString());
-                return false;
-            }
-        }
-        return false;
-    }
-
-    private Object report1() throws Exception
-    {
-        Method method = TelephonyManager.class.getMethod(
-            "getPreferredNetworkType", int.class);
-        method.setAccessible(true);
-        return method.invoke(getManager(), getSubId());
-    }
-
-    private Object report2() throws Exception
-    {
-        Method method = TelephonyManager.class.getMethod(
-            "getPreferredNetworkType");
-        method.setAccessible(true);
-        return method.invoke(getManager());
-    }
-
-    private Object execute1(int type) throws Exception
-    {
-        Method method = TelephonyManager.class.getMethod(
-            "setPreferredNetworkType", int.class, int.class);
-        method.setAccessible(true);
-        return method.invoke(getManager(), getSubId(), type);
-    }
-
-    private Object execute2(int type) throws Exception
-    {
-        Method method = TelephonyManager.class.getMethod(
-                "setPreferredNetworkType", int.class);
-        method.setAccessible(true);
-        return method.invoke(getManager(), type);
-    }
-
-    private TelephonyManager getManager()
-    {
-        return (TelephonyManager)getSystemService(TELEPHONY_SERVICE);
-    }
-
-    private int getSubId()
-    {
-        try
-        {
-            Method method =
-                TelephonyManager.class.getDeclaredMethod("getSubId");
-            return (Integer)method.invoke(getManager());
-        }
-        catch (Exception ex)
-        {
-            Log.e(TAG, "Failed to get sub id: " + ex.toString());
-            return Integer.MAX_VALUE;
-        }
-    }
+    return false;
+  }
 }
